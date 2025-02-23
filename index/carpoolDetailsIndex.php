6aa74b8b264bb1323e50a0877f3fc7f8faad6caf
SoCode9 <tennis00015@gmail.com>
2025-02-23 22:35:52 +0100

    add static first block (Travel's details and booking block)

---
+++ index/carpoolDetailsIndex.php
@@ -0,0 +1,26 @@
+<?php
+
+//add back ?
+
+?>
+
+<?php include '../templates/header.php'; ?>
+<html>
+<style>
+    /*Adapt Login button*/
+    #carpoolButton {
+        color: #F2C674;
+    }
+
+    /*?? add a hover on login button ??*/
+</style>
+
+</html>
+<?php
+
+?>
+<div>
+    <?php include '../templates/carpoolDetails.php';
+    ?>
+
+</div>
