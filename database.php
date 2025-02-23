2110f9955b5b753966d3669ba17fc6984bf47e2c
SoCode9 <tennis00015@gmail.com>
2025-02-23 14:01:18 +0100

    add file database.php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+//CONNECTION TO THE DATABASE
+
+$host = "localhost";
+$dbname = "ecoride";
+$port = 3307;
+$username = "root"; // Modifier selon ton environnement
+$password = ""; // Modifier si besoin
+
+try {
+    //création de l'objet PDI pour la connexion à MySql
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+    //configuration des erreurs PDO en mode Exception (utile pour le débogage). Active le mode exception pour afficher les erreurs SQL.
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) { // Capture les erreurs et affiche un message.
+    //gestion des erreurs de connexion
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+?>
