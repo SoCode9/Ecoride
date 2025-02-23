f5fb92d9161df7f5c95650132ef23c89b0c5bd41
SoCode9 <tennis00015@gmail.com>
2025-02-23 14:38:57 +0100

    add files for login form page

---
+++ index/formLoginIndex.php
@@ -0,0 +1,25 @@
+<?php
+require_once "back/formLoginBack.php";
+?>
+
+<?php include 'templates/header.php'; ?>
+<html>
+<style>
+    /*Adapt Login button*/
+    #loginButton {
+        background-color: #F2C674;
+        color: black;
+        border: #F2C674;
+    }
+
+    /*?? add a hover on login button ??*/
+</style>
+
+</html>
+
+<div>
+    <?php include 'templates/formLogin.php'; //PENSER à CHANGER L'ID PSEUDO ETC AVEC CREATEpSEUDO CAR AURA BESOIN DE ID POUR FORMULAIRE DE CONNEXION
+    ?>
+    <!--MANQUE FORM LOGIN-->
+
+</div>
---
+++ templates/formLogin.php
@@ -0,0 +1,17 @@
+<head>
+
+    <title>Connexion</title>
+
+</head>
+
+<form action="" method="POST">
+    <label for="pseudo">Pseudo : </label>
+    <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" required>
+    <label for="mail">Email : </label>
+    <input type="email" id="mail" name="mail" required>
+    <label for="password">Mot de passe : </label>
+    <input type="password" id="password" name="password" required>
+    <label for="chauffeur">Chauffeur ? : </label>
+    <input type="checkbox" id="chauffeur" name="chauffeur">
+    <input type="submit" value="Valider ">
+</form>
