5cb9c232fbbd9f406124cc68dc2859ac4fef5fbf
SoCode9 <tennis00015@gmail.com>
2025-03-09 22:16:06 +0100

    add double confirmation with updateParticipation.php

--- database.php
+++ database.php
@@ -26,7 +26,7 @@ try {
 
 
 //A METTRE LORS DE LA CONNEXION
-    $_SESSION['user_id'] = /* (int) 3 */ null; //remplacer par $user['id'] quand ok
+    $_SESSION['user_id'] = (int) 3 /* null */; //remplacer par $user['id'] quand ok
 //$_SESSION['user_pseudo'] = $user['pseudo'];
 //$_SESSION['user_credit'] = 10; // Ajouter des crédits pour le test
 
