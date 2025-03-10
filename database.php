6316aa07041d6292d525850253b57dd351a4b445
SoCode9 <tennis00015@gmail.com>
2025-03-10 22:07:08 +0100

    replace port 3307 by 3308

--- database.php
+++ database.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 $host = "localhost";
 $dbname = "ecoride";
-$port = 3307;
+$port = 3308;
 $username = "root"; // Modifier selon ton environnement
 $password = ""; // Modifier si besoin
 
