23cb8258b3c6034b84f101dfa26ba3dbe99243c9
SoCode9 <tennis00015@gmail.com>
2025-03-12 12:05:18 +0100

    add feature to login in, with error messages

--- back/loginPageBack.php
+++ back/loginPageBack.php
@@ -8,29 +8,55 @@ require_once "../class/User.php";
 //CONNECTION WITH DATABASE WHEN AN ACCOUNT IS CREATE
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $pseudo = $_POST['pseudo'];
-        $mail = $_POST['mail'];
-        $password = $_POST['password'];
+    if ($_POST['action'] === "createAccount") {
+        try {
+            $pseudo = $_POST['pseudo'];
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
 
-        // User creation
-        $newUser = new User($pdo, null, $pseudo, $mail, $password, $chauffeur);
+            // User creation
+            $newUser = new User($pdo, null, $pseudo, $mail, $password, $chauffeur);
 
-        // Check if user is created well
-        if ($newUser->saveUserToDatabase()) {
-            $_SESSION['success_message'] = 'Compte créé avec succès ! Vous avez été crédité de 20 crédits 🎉';
-            $_SESSION['user_id'] = $newUser->getId();
-            header('Location: carpoolSearchIndex.php');
+            // Check if user is created well
+            if ($newUser->saveUserToDatabase()) {
+                $_SESSION['success_message'] = 'Compte créé avec succès ! Vous avez été crédité de 20 crédits 🎉';
+                $_SESSION['user_id'] = $newUser->getId();
+                header('Location: carpoolSearchIndex.php');
+                exit();
+            } else {
+                throw new Exception("Erreur lors de l'enregistrement en base de données.");
+            }
+        } catch (Exception $e) {
+            $_SESSION['error_message'] = $e->getMessage();
+            header('Location: loginPageIndex.php'); // Redirect to login page
             exit();
-        } else {
-            throw new Exception("Erreur lors de l'enregistrement en base de données.");
         }
-    } catch (Exception $e) {
-        $_SESSION['error_message'] = $e->getMessage();
-        header('Location: loginPageIndex.php'); // Redirect to login page
-        exit();
+    }
+
+    if ($_POST['action'] === "formLogin") {
+        try {
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
+
+            $searchUser = new User($pdo, null, null, $mail, $password, null);
+
+            if ($searchUser->searchUserInDB($mail, $password)) {
+                $_SESSION['success_message'] = 'Connexion réussie !';
+                $_SESSION['user_id'] = $searchUser->getId();
+                header('Location: carpoolSearchIndex.php');
+                exit();
+            } else {
+                throw new Exception("Erreur lors de la connexion de l'utilisateur");
+            }
+        } catch (Exception $e) {
+            $_SESSION['error_message'] = $e->getMessage();
+            header('Location: loginPageIndex.php'); // Redirect to login page
+            exit();
+        }
     }
 }
+
+
 
 
 ?>
