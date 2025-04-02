4f78f3762c5fd344fdf55b50fdfb594d973b31d5
SoCode9 <tennis00015@gmail.com>
2025-04-02 15:31:27 +0200

    add static block validate ratings

--- templates/employeeSpace.php
+++ templates/employeeSpace.php
@@ -1,3 +1,36 @@
-<?php
+<main>
+    <div class="flex-row gap-12 m-tb-12 m-8">
+        <span>Pseudo</span>
+        <span>mail@exemple.com</span>
+    </div>
+    <div class="half-separation">
+        <section class="m-8  flex-column gap-12">
+            <h2 class="subTitleGreen">Valider les avis des participants</h2>
+                <div class="flex-column gap-8 block-light-grey">
+                    <div class="flex-row flex-between ">
+                        <span>Pseudo passager</span>
+                        <div class="flex-row gap-8">
+                            <button class="btn bg-light-green">Valider</button>
+                            <button class="btn bg-light-red">Refuser</button>
+                        </div>
+                    </div>
+                    <div class="flex-row flex-between">
+                        <span>J'ai adoré le voyage, la discussion était fluide et agréable</span>
+                        <div class="flex-row m-8">
+                            <img src="..\icons\EtoileJaune.png" class="imgFilter" alt="Icone étoile">
+                            <span>5</span>
+                        </div>
+                    </div>
+                    <div class="flex-row flex-between">
+                        <div class="flex-row gap-4">
+                            <img src="..\icons\Voiture.png" class="imgFilter" alt="Icone voiture">
+                            <span>Pseudo chauffeur</span>
+                        </div>
+                        <span class="italic font-size-very-small">03/07/2025</span>
 
-echo "Employee space page";
+                    </div>
+                </div>
+                <hr>
+        </section>
+    </div>
+</main>
