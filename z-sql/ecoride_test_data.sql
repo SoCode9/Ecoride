19db3c7f680b3ff8934f4b093b17e9d914ac8471
Solène <tennis00015@gmail.com>
2025-08-30 11:32:45 +0200

    edit ecoride_test_data.sql for local importation

--- z-sql/ecoride_test_data.sql
+++ z-sql/ecoride_test_data.sql
@@ -2,7 +2,6 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: mysql-ecoride-sge.alwaysdata.net
 -- Generation Time: May 14, 2025 at 03:46 PM
 -- Server version: 10.11.11-MariaDB
 -- PHP Version: 7.4.33
@@ -18,8 +17,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ecoride-sge_ecoride`
+-- Database: `ecoride`
 --
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `mail`, `password`, `credit`, `photo`, `id_role`, `is_activated`, `created_at`) VALUES
+('13a40eaa-2802-11f0-9346-5254007e02a0', 'test2', 'test2@test.com', '$2y$10$gUdrhbMvjEay.x.zoX1vruNgmqlnm6WRcEBsrCHpnBxNf0.hRN9Qm', 11, NULL, 3, 1, '2025-05-03 09:36:23'),
+('9a17f210-2807-11f0-9346-5254007e02a0', 'test3', 'test3@test.com', '$2y$10$RgsawT.7ziuSm15o7e8XFed9J3WPJVNspR2wUbEMJ.xlMvsaLZXs2', 14, NULL, 2, 1, '2025-05-03 10:15:56'),
+('ad1c3e3f-26c9-11f0-9b59-5254007e02a0', 'test1', 'test1@test.com', '$2y$10$JIu61frhdhKCADoIw0fJn.qhXABw8dT1pdvuVko7rDrc9ugl3HpnK', 20, NULL, 1, 1, '2025-05-01 20:20:08'),
+('ae429bfa-290a-11f0-b09b-5254007e02a0', 'employee1', 'employee1@test.com', '$2y$10$2.Q/mk8T3YAGXSNeDm9YV.3.kNGBm9pOykyaF5h1xxjKoGh84Fs4a', NULL, NULL, 4, 1, '2025-05-04 17:10:29');
+
+--
+-- Dumping data for table `driver`
+--
+
+INSERT INTO `driver` (`user_id`, `food`, `music`, `pets`, `smoker`, `speaker`, `add_pref_1`, `add_pref_2`, `add_pref_3`) VALUES
+('13a40eaa-2802-11f0-9346-5254007e02a0', NULL, NULL, 1, 1, NULL, NULL, NULL, NULL),
+('9a17f210-2807-11f0-9346-5254007e02a0', 0, NULL, 1, 0, NULL, 'Privilégier les petits bagages', NULL, NULL);
 
 --
 -- Dumping data for table `cars`
@@ -30,12 +47,13 @@ INSERT INTO `cars` (`car_id`, `brand_id`, `driver_id`, `car_licence_plate`, `car
 (2, 3, '13a40eaa-2802-11f0-9346-5254007e02a0', 'OT-222-BH', '1999-05-22', 2, '0992', 'gris', 1);
 
 --
--- Dumping data for table `driver`
+-- Dumping data for table `travels`
 --
 
-INSERT INTO `driver` (`user_id`, `food`, `music`, `pets`, `smoker`, `speaker`, `add_pref_1`, `add_pref_2`, `add_pref_3`) VALUES
-('13a40eaa-2802-11f0-9346-5254007e02a0', NULL, NULL, 1, 1, NULL, NULL, NULL, NULL),
-('9a17f210-2807-11f0-9346-5254007e02a0', 0, NULL, 1, 0, NULL, 'Privilégier les petits bagages', NULL, NULL);
+INSERT INTO `travels` (`id`, `car_id`, `driver_id`, `travel_date`, `travel_departure_city`, `travel_departure_time`, `travel_arrival_city`, `travel_arrival_time`, `travel_price`, `travel_description`, `travel_status`, `validated_at`, `created_at`) VALUES
+('4b7b3901-290b-11f0-b09b-5254007e02a0', 2, '13a40eaa-2802-11f0-9346-5254007e02a0', '2025-05-04', 'Saint-Julien-en-Genevois', '19:00:00', 'Lyon', '20:00:00', 3, '', 'ended', '2025-05-04', '2025-05-04 17:14:53'),
+('67d906cc-2905-11f0-b09b-5254007e02a0', 1, '9a17f210-2807-11f0-9346-5254007e02a0', '2025-09-25', 'Saint-Julien-en-Genevois', '12:00:00', 'Lyon', '14:00:00', 11, 'Merci d\'être à l\'heure au rdv devant l\'église', 'not started', NULL, '2025-05-04 16:32:44'),
+('af8ed414-2ab9-11f0-9ca7-5254007e02a0', 2, '13a40eaa-2802-11f0-9346-5254007e02a0', '2025-05-06', 'Saint-Julien-en-Genevois', '22:35:00', 'Lyon', '23:55:00', 3, '', 'ended', '2025-05-06', '2025-05-06 20:35:45');
 
 --
 -- Dumping data for table `ratings`
@@ -54,24 +72,8 @@ INSERT INTO `reservations` (`id`, `user_id`, `travel_id`, `is_validated`, `credi
 (4, '13a40eaa-2802-11f0-9346-5254007e02a0', '67d906cc-2905-11f0-b09b-5254007e02a0', 0, 11, NULL, NULL),
 (5, '9a17f210-2807-11f0-9346-5254007e02a0', 'af8ed414-2ab9-11f0-9ca7-5254007e02a0', 1, 3, NULL, NULL);
 
---
--- Dumping data for table `travels`
---
 
-INSERT INTO `travels` (`id`, `car_id`, `driver_id`, `travel_date`, `travel_departure_city`, `travel_departure_time`, `travel_arrival_city`, `travel_arrival_time`, `travel_price`, `travel_description`, `travel_status`, `validated_at`, `created_at`) VALUES
-('4b7b3901-290b-11f0-b09b-5254007e02a0', 2, '13a40eaa-2802-11f0-9346-5254007e02a0', '2025-05-04', 'Saint-Julien-en-Genevois', '19:00:00', 'Lyon', '20:00:00', 3, '', 'ended', '2025-05-04', '2025-05-04 17:14:53'),
-('67d906cc-2905-11f0-b09b-5254007e02a0', 1, '9a17f210-2807-11f0-9346-5254007e02a0', '2025-09-25', 'Saint-Julien-en-Genevois', '12:00:00', 'Lyon', '14:00:00', 11, 'Merci d\'être à l\'heure au rdv devant l\'église', 'not started', NULL, '2025-05-04 16:32:44'),
-('af8ed414-2ab9-11f0-9ca7-5254007e02a0', 2, '13a40eaa-2802-11f0-9346-5254007e02a0', '2025-05-06', 'Saint-Julien-en-Genevois', '22:35:00', 'Lyon', '23:55:00', 3, '', 'ended', '2025-05-06', '2025-05-06 20:35:45');
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `pseudo`, `mail`, `password`, `credit`, `photo`, `id_role`, `is_activated`, `created_at`) VALUES
-('13a40eaa-2802-11f0-9346-5254007e02a0', 'test2', 'test2@test.com', '$2y$10$gUdrhbMvjEay.x.zoX1vruNgmqlnm6WRcEBsrCHpnBxNf0.hRN9Qm', 11, NULL, 3, 1, '2025-05-03 09:36:23'),
-('9a17f210-2807-11f0-9346-5254007e02a0', 'test3', 'test3@test.com', '$2y$10$RgsawT.7ziuSm15o7e8XFed9J3WPJVNspR2wUbEMJ.xlMvsaLZXs2', 14, NULL, 2, 1, '2025-05-03 10:15:56'),
-('ad1c3e3f-26c9-11f0-9b59-5254007e02a0', 'test1', 'test1@test.com', '$2y$10$JIu61frhdhKCADoIw0fJn.qhXABw8dT1pdvuVko7rDrc9ugl3HpnK', 20, NULL, 1, 1, '2025-05-01 20:20:08'),
-('ae429bfa-290a-11f0-b09b-5254007e02a0', 'employee1', 'employee1@test.com', '$2y$10$2.Q/mk8T3YAGXSNeDm9YV.3.kNGBm9pOykyaF5h1xxjKoGh84Fs4a', NULL, NULL, 4, 1, '2025-05-04 17:10:29');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
