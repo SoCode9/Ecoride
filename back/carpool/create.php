252ba6725b5fab36c1cbae13dd62b430bd397eea
Solène <tennis00015@gmail.com>
2025-09-04 21:20:55 +0200

    remove the require_once to the Driver class

--- back/carpool/create.php
+++ back/carpool/create.php
@@ -3,7 +3,6 @@ if (session_status() === PHP_SESSION_NONE)
     session_start();
 
 require_once __DIR__ . "/../../database.php";
-require_once __DIR__ . "/../../class/Driver.php";
 require_once __DIR__ . "/../../class/Travel.php";
 
 $pdo = pdo();
